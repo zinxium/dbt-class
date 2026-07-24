be2b229730106dd739df2da5231a5ebcd55e2a4f
Cynthia Zinsou <mayazins44@gmail.com>
2026-07-24 07:49:06 +0000

    made corrections

--- models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -1,1 +1,13 @@
-select id as customer_id, first_name, last_name from raw.jaffle_shop.customers
+with source as (
+    select * from {{ source('jaffle_shop', 'customers') }}
+),
+
+renamed as (
+    select
+        id as customer_id,
+        first_name,
+        last_name
+    from source
+)
+
+select * from renamed
--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -1,7 +1,16 @@
+with
+   source as (
+       select * from {{ source('jaffle_shop', 'orders')}} 
+),
+
+renamed as (
     select
         id as order_id,
         user_id as customer_id,
         order_date,
-        status
+        status as order_status
     
-    from raw.jaffle_shop.orders
+    from source
+)
+
+select * from renamed
